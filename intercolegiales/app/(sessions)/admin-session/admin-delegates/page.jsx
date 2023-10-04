39465a70eba4f1f30e0d5fa40de81c88d6ce5831
Sebastian Collado <sebastiancolladott@gmail.com>
2023-10-04 00:20:05 -0300

    eslint and prettier update config

--- intercolegiales/app/(sessions)/admin-session/admin-delegates/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h3>Administrar delegados</h3>;
-}
---
+++ intercolegiales/src/app/(sessions)/professor-session/page.jsx
@@ -0,0 +1,24 @@
+import Menu from '@/app/components/Menu'
+
+export default function Page() {
+  let options = [
+    {
+      title: 'Completar tabla de alumnos escolar',
+      link: '/professor-sesion/scholar-table'
+    },
+    {
+      title: 'Inscribir alumnos',
+      link: '/professor-sesion/enroll-students'
+    },
+    {
+      title: 'Modificar planillas',
+      link: '/professor-sesion/modify-sheets'
+    }
+  ]
+
+  return (
+    <>
+      <Menu title={'Menu Profesor'} options={options} />
+    </>
+  )
+}
